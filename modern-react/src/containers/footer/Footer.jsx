b46ef370bd79cacb9e87839735673f0b5f460108
CarbonDRFT <Danier-1995@hotmail.com>
2022-09-20 13:04:44 +0200

    Front-end react website completed

--- modern-react/src/containers/footer/Footer.jsx
+++ modern-react/src/containers/footer/Footer.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
 import './footer.css'
+import gpt3Logo from '../../assets/logo.svg'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className='gpt3__footer section__padding'>
+      <div className="gpt3__footer-heading">
+        <h1 className="gradient__text">Do you want to step in to the future before others</h1>
+      </div>
+      <div className="gpt3__footer-btn">
+        <p>Request Early Access</p>
+      </div>
+
+      <div className="gpt3__footer-links">
+        <div className="gpt3__footer-links_logo">
+          <img src={gpt3Logo} alt="logo" />
+          <p>Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Comapny</h4>
+          <p>Terms & Condition</p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Get in touch</h4>
+          <p>Oslogate, 1234 Oslo</p>
+          <p>+47 000 00 000</p>
+          <p>danier-1995@hotmail.com</p>
+          
+        </div>
+      </div>
+      <div className="gpt3__footer-copyright">
+        <p>© 2021 GPT-3. All rights reserved.</p>
+      </div>
+    </div>
   )
 }
 
